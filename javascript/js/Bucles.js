c3e5af84d37225d295ca1dc2d8c3fa41184ec7cb
Salvacll <calosa_aliesma24@iesmedinaazahara.es>
2026-05-12 11:22:34 +0200

    a

---
+++ javascript/js/Bucles.js
@@ -0,0 +1,23 @@
+
+
+
+function multiplicar (numero)
+{
+
+        let j=0;
+        while (j<=10){
+            console.log(j + "x" + numero + "=" + j*numero);
+            j++;
+        }
+
+
+}
+
+
+let i;
+for(i=0;i<=10;i++){
+
+    multiplicar(i);
+    
+
+}
---
+++ javascript/sintaxi.js
@@ -0,0 +1,7 @@
+let nombre="Salva";
+let year=2026;
+const PI=3.1416;
+console.log(nombre, year)
+
+Nombre="prueba"
+console.log(Nombre, year)
